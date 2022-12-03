a14fe7f327ae9afde9ff995ac8c50b5039e8640a
jackvdp <jack@pumpymusic.co.uk>
2022-12-03 20:31:05 +0000

    Initial Commit

---
+++ chatGDPMenuBar/chatGDPMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatGDPMenuBarApp.swift
+//  chatGDPMenuBar
+//
+//  Created by Jack Vanderpump on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct chatGDPMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
