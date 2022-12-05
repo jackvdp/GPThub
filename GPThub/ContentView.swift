c2db8e8cf80a15bf5b8b59044d7e17f431d3f77d
jackvdp <jack@pumpymusic.co.uk>
2022-12-05 10:41:55 +0000

    Remove dock icon and add button to access settings

--- GPThub/ContentView.swift
+++ GPThub/ContentView.swift
@@ -13,9 +13,38 @@ struct ContentView: View {
     @AppStorage(K.Storage.frameWidth) var frameWidth: Double = K.Storage.defaultWidth
     
     var body: some View {
+        ZStack(alignment: .topTrailing) {
+            gptView
+            settingsButton
+        }
+    }
+    
+    var gptView: some View {
         WebView()
             .frame(width: (NSScreen.main?.frame.width ?? 1000) * frameWidth,
                    height: (NSScreen.main?.frame.height ?? 1000) * frameHeight)
+    }
+    
+    var settingsButton: some View {
+        Button(action: openSettingsWindow) {
+            buttonView
+        }
+        .buttonStyle(.plain)
+    }
+    
+    var buttonView: some View {
+        Image(systemName: "gear")
+            .foregroundColor(.white.opacity(0.8))
+            .padding(10)
+            .font(.title)
+            .background(.indigo.opacity(0.7))
+            .clipShape(Capsule())
+            .padding(10)
+            .shadow(radius: 20)
+    }
+    
+    func openSettingsWindow() {
+        NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
     }
 }
 
