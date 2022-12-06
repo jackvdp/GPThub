dbded822208a2a7cbb65e489a8cda61f7ebd7d75
jackvdp <jack@pumpymusic.co.uk>
2022-12-06 12:47:53 +0000

    Add more detailed menu

--- GPThub/ContentView.swift
+++ GPThub/ContentView.swift
@@ -11,13 +11,16 @@ struct ContentView: View {
     
     @AppStorage(K.Storage.frameHeight) var frameHeight: Double = K.Storage.defaultHeight
     @AppStorage(K.Storage.frameWidth) var frameWidth: Double = K.Storage.defaultWidth
+    @State private var showMenu = false
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
             gptView
-            settingsButton
+            buttons
         }
     }
+    
+    // MARK: - Components
     
     var gptView: some View {
         WebView()
@@ -25,28 +28,52 @@ struct ContentView: View {
                    height: (NSScreen.main?.frame.height ?? 1000) * frameHeight)
     }
     
-    var settingsButton: some View {
-        Button(action: openSettingsWindow) {
-            buttonView
+    var buttons: some View {
+        HStack(spacing: 0) {
+            if showMenu {
+                button("gear", action: openSettingsWindow)
+                    .transition(.move(edge: .trailing))
+                button("power", action: killApp)
+                    .transition(.move(edge: .trailing))
+            }
+            button(showMenu ? "xmark" : "line.3.horizontal",
+                   action: openMenu)
+        }
+    }
+    
+    func button(_ image: String, action: @escaping ()->()) -> some View {
+        Button(action: action) {
+            Image(systemName: image)
+                .foregroundColor(.white.opacity(0.8))
+                .font(.title2)
+                .frame(width: 30, height: 30)
+                .padding(10)
+                .background(.indigo.opacity(0.7))
+                .clipShape(Circle())
+                .shadow(radius: 20)
+                .padding(10)
         }
         .buttonStyle(.plain)
     }
     
-    var buttonView: some View {
-        Image(systemName: "gear")
-            .foregroundColor(.white.opacity(0.8))
-            .padding(10)
-            .font(.title)
-            .background(.indigo.opacity(0.7))
-            .clipShape(Capsule())
-            .padding(10)
-            .shadow(radius: 20)
+    // MARK: - Actions
+    
+    func openMenu() {
+        withAnimation {
+            showMenu.toggle()
+        }
     }
     
     func openSettingsWindow() {
         NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
     }
+    
+    func killApp() {
+        NSApp.terminate(self)
+    }
 }
+
+// MARK: - Previews
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
